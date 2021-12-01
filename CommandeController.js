2d3adfe66b614f7ca920955bdf0e0e20aaccc7b3
RootKoro <65048662+RootKoro@users.noreply.github.com>
2021-12-01 17:57:10 +0000

    Update CommandeController.js

--- CommandeController.js
+++ CommandeController.js
@@ -4,38 +4,6 @@ const Commande = use('App/Models/Commande')
 const QuantiteArticleCommande = use('App/Models/QuantiteArticleCommande')
 
 class CommandeController {
-    /**
-     * @swagger
-     * /commande:
-     *   get:
-     *     tags:
-     *       - API commande
-     *     summary: renvoi toutes les commande
-     *     responses:
-     *       200:
-     *         description: renvoi la liste des commande enregistres
-     */
-    async index() { return Commande.all() }
-
-    /**
-     * @swagger
-     * /commande:
-     *   post:
-     *     tags:
-     *       - API commande
-     *     summary: enregistre une nouvelle commande
-     *     parameters:
-     *         - name: commande
-     *           in: body
-     *           required: true
-     *           schema:
-     *              $ref: "#/definitions/Commande"
-     *     responses:
-     *       201:
-     *         description: Commande enregistree avec succes
-     *       500:
-     *         description: une Erreur s'est produite lors de la creation de la commande, verifiez les erreurs
-     */
     async store({ request, response }) {
         try {
             const total = request.input('total_commande')
@@ -56,45 +24,13 @@ class CommandeController {
                 status: stat,
                 articles: text
             })
-
-            /* try {
-                const articles_ = await request.input('articles')
-                articles_.forEach(async function(item) {
-                    await ArticleCommande.create({
-                        id_article: item.id,
-                        quantite_article_commande: item.quantite,
-                        id_commande: commande_.$attributes.id
-                    })
-                })
-            } catch (error) {
-                return error
-            } */
             return response.status(202).json(commande_)
         } catch (error) {
             console.log(error)
             return response.status(500).send('Stockage impossible, veuillez reessayer!')
         }
     }
-
-    /**
-     * @swagger
-     * /commande/{id}:
-     *   get:
-     *     tags:
-     *       - API commande
-     *     summary: renvois la commande correspondante a l'identifiant
-     *     parameters:
-     *         - name: id
-     *           desciption: identifiant
-     *           in: path
-     *           schema:
-     *              type: number
-     *     responses:
-     *       200:
-     *         description: Commande renvoyee avec succes
-     *       400:
-     *         description: Aucun resultat
-     */
+    
     async show({ response, params }) {
         try {
             const commande_ = await Commande.findOrFail(params.id)
@@ -105,25 +41,6 @@ class CommandeController {
         }
     }
 
-    /**
-     * @swagger
-     * /commande/info/{id}:
-     *   get:
-     *     tags:
-     *       - API commande
-     *     summary: renvois plus d'info sur une commande
-     *     parameters:
-     *         - name: id
-     *           desciption: identifiant
-     *           in: path
-     *           schema:
-     *              type: number
-     *     responses:
-     *       200:
-     *         description: More info
-     *       400:
-     *         description: Aucun resultat
-     */
     async showQuantityArticlesOrdered({ params }) {
         try {
             const commande_ = await Commande.findOrFail(params.id)
@@ -139,73 +56,6 @@ class CommandeController {
         } catch (error) {
             console.log(error)
             return error
-        }
-    }
-
-    /**
-     * @swagger
-     * /commande/{id}:
-     *   put:
-     *     tags:
-     *       - API commande
-     *     summary: met a jour une commande
-     *     parameters:
-     *         - name: id
-     *           desciption: identifiant
-     *           in: path
-     *           schema:
-     *              type: number
-     *         - name: commande
-     *           in: body
-     *           required: true
-     *           schema:
-     *              $ref: "#/definitions/Commande"
-     *     responses:
-     *       202:
-     *         description: commande mis a jour avece succes
-     *       500:
-     *         description: une erreur s'est produite lors de la mise a jour
-     */
-    async update({ request, response, params }) {
-        try {
-            const commande_ = await Commande.findOrFail(params.id)
-            commande_.total_commande = request.input('total_commande')
-            commande_.mode_paiement = request.input('mode_paiement')
-            commande_.id_client = request.input('id_client')
-            commande_.status = request.input('status')
-            commande_.save()
-            return response.status(202).json(commande_)
-        } catch (error) {
-            return response.status(500).send('Echec de la mise a jour, veuillez reessayer!')
-        }
-    }
-
-    /**
-     * @swagger
-     * /commande/{id}:
-     *   delete:
-     *     tags:
-     *       - API commande
-     *     summary: supprime une commande
-     *     parameters:
-     *         - name: id
-     *           desciption: identifiant
-     *           in: path
-     *           schema:
-     *              type: number
-     *     responses:
-     *       203:
-     *         description: commande supprimee avec succes
-     *       400:
-     *         description: aucune commande ne correspond a cet identifiant
-     */
-    async destroy({ params, response }) {
-        try {
-            const commande_ = await Commande.find(params.id)
-            await commande_.delete()
-            return response.status(203).send('suppression reussie')
-        } catch (error) {
-            return response.status(500).send('Aucun resultat ne correspond a cet id')
         }
     }
 }
